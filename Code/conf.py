2a902faab688788cd26ad63060eb26636eafcdd0
Anthony S. A <asalazar0592@gmail.com>
2019-10-11 18:15:42 -0600

    Avance 11-10-2'019

--- Code/conf.py
+++ Code/conf.py
@@ -1,29 +1,15 @@
+import constants
 import pymssql
 import psycopg2
-
-#conectionToSQL
-_sql_server = "localhost"
-_sql_database = "B89694"
-_sql_server_port = 1433
-_sql_user = "antony"
-_sql_password = "1992"
-
-
-#conectionToPostgreSQL
-_postgre_server = "localhost"
-_postgre_database = "name"
-_postgre_server_port = "5432"
-_postgre_user = "postgres"
-_postgre_password = "postgres"
 
 #SQL Connection
 def mssql_connection():
     try:
-        cnx=pymssql.connect(server=_sql_server,
-                           port=_sql_server_port,
-                           user=_sql_user,
-                           password=_sql_password,
-                           database=_sql_database)
+        cnx=pymssql.connect(server=constants._sql_server,
+                           port=constants._sql_server_port,
+                           user=constants._sql_user,
+                           password=constants._sql_password,
+                           database=constants._sql_database)
         return cnx
     except:
         print ('Error MSSQL connection')
@@ -31,46 +17,26 @@ def mssql_connection():
 #Postgre Connection
 def postgreSql_Connection():
     try:
-        cnx=psycopg2.connect("host="+_postgre_server+
-                            "dbname="+_postgre_database+
-                            "user="+_postgre_user+
-                            "password="+_postgre_password+
-                            "port="+_postgre_server_port)
+        cnx=psycopg2.connect("host="+constants._postgre_server+
+                            "dbname="+constants._postgre_database+
+                            "user="+constants._postgre_user+
+                            "password="+constants._postgre_password+
+                            "port="+constants._postgre_server_port)
         return cnx
     except:
-        print ('Error postgre connection')
+        print ('Error PostgreSQL connection')
 
-
+#Extracting Data
 def get_data_from_sql(sp):
     try:
         con=mssql_connection()
         cur=con.cursor()
-        cur.execute("")
+        cur.execute("execute {} @output_IS_SUCCESSFUL=0, @output_STATUS=0".format(sp))
+        data_return = cur.fetchall()
         con.commit()
         
-        return
-     except IOError as e:
-        print ('Error {0} Getting data from MSSQL: {1}'. format(e.errno,e.strerror))    
+        return data_return
+
+    except IOError as e:
+        print ("Error {0} Getting data from MSSQL: {1}". format(e.errno, e.strerror))    
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
